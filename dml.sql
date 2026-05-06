7a7571855f9d82cced49d76edeeff39a04822f38
Luis <blackarner@gmail.com>
2026-05-05 21:30:04 -0300

    Create dml.sql

---
+++ dml.sql
@@ -0,0 +1,4 @@
+INSERT INTO users (name, email, active) VALUES 
+('Luis Felipe', 'luisrc10@gmail.com', TRUE),
+('Carlos Eduardo', 'cadulol1@gmail.com', FALSE),
+('Luis Fernando', 'nando1030@gmail.com', TRUE);
